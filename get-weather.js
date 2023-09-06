d7d44a07df787df09fb4e635b33c8b0af74867c6
Andrew Alsing <andrew32alsing@gmail.com>
2023-09-06 11:47:53 -0700

    removed unnecessary getWeather function

--- get-weather.js
+++ get-weather.js
@@ -1,18 +1,5 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
-export async function getWeather(apikey, zip, units = "imperial") {
-  const apiKey = `${apikey}`;
-  const path = `${BASE_URL}zip=${zip}&appid=${apiKey}&units=${units}`;
-
-  const res = await fetch(path);
-  const data = await res.json();
-
-  return {
-    temperature: data.main.temp,
-    description: data.weather[0].description,
-  };
-}
-
 export async function getWeatherByZip(apikey, zip, unit = "imperial") {
   const path = `${BASE_URL}zip=${zip}&appid=${apikey}&units=${unit}`;
   const res = await fetch(path);
